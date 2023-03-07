1b7430934c5958be521e94ac0b0a42e261eaefb1
Abhijeet Kashide <92225416+abhiinyou@users.noreply.github.com>
2023-03-08 00:24:09 +0530

    new-api-key-updated

--- client/script.js
+++ client/script.js
@@ -84,6 +84,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   const response = await fetch("https://codebot-9mk2.onrender.com", {
+    
     method: "POST",
     headers: {
       "Content-Type": "application/json",
